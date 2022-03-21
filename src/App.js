e5031ec5a20e8b630d5c3359dd3923cde6bdc496
Dario Mercuri <dario.mercuri31@gmail.com>
2022-03-20 23:54:54 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import { Header } from "./Components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { Home } from "./Components/Home";
+import { Blog } from "./Components/Blog";
+import { Login } from "./Components/Login";
+import { Register } from "./Components/Register";
+import { Post } from "./Components/Post";
+import { Create } from "./Components/Create";
+import { Edit } from "./Components/Edit";
 
 function App() {
-  return (
-    <Header/>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/blog" element={<Blog />} />
+                <Route exact path="/login" element={<Login />} />
+                <Route exact path="/register" element={<Register />} />
+                <Route exact path="/create" element={<Create />} />
+                <Route exact path="/post/:id" element={<Post />} />
+                <Route exact path="/edit/:id" element={<Edit />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
